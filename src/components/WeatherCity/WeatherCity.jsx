5efb791d6f4bec217a286599bb34caad64e9c0ff
AbrahamEmilio <emeilio97@outlook.com>
2024-06-06 15:43:43 -0600

    cambios

--- src/components/WeatherCity/WeatherCity.jsx
+++ src/components/WeatherCity/WeatherCity.jsx
@@ -5,71 +5,66 @@ const API_KEY = '5c1c969d600654194e2c5b11a4e63a4c';
 
 function WeatherCity (){
 
+    const [weatherCity, setWeatherCity] = useState()
+
     let lat;
     let lon;
-    let weatherCity;
-    let sky;
 
-    let weatherWeek = [
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Monday'
-        },
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Tuesday'
-        },
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Wednesday'
-        },
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Thursday'
-        },
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Saturday'
-        },
-        {
-            nameCity: 'Dallas',
-            weather: 32,
-            day: 'Sunday'
-        }
-    ];
+    // let weatherWeek = [
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Monday'
+    //     },
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Tuesday'
+    //     },
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Wednesday'
+    //     },
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Thursday'
+    //     },
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Saturday'
+    //     },
+    //     {
+    //         nameCity: 'Dallas',
+    //         weather: 32,
+    //         day: 'Sunday'
+    //     }
+    // ];
 
     const getCoordinates = async() => {
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q='dallas'&appid=${API_KEY}`)
         const data = await response.json()
-        lat = data[0].lat.toFixed(2);
-        lon = data[0].lon.toFixed(2);
-
-        getData(lat, lon);
-    }
-
-    const getData = async() => {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-        const data = await response.json();
+        console.log(data)
         
-        weatherCity = data;
-        sky = weatherCity.weather[0].main;
+        // lat = data[0].lat.toFixed(2);
+        // lon = data[0].lon.toFixed(2);
 
-        // if(data.cod !== '200'){
-        //     console.log(data.cod)
-        // } else (
-        //     console.log(data)
-        // )
+        // getData(lat, lon);
     }
+
+    // const getData = async() => {
+    //     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    //     const data = await response.json();
+
+    //     setWeatherCity(data);
+    // }
 
     getCoordinates();
 
-    const [weather, setWeather] = useState(weatherWeek)
 
+    
     return(
         <>
             <div className='weatherCity'>
@@ -81,7 +76,7 @@ function WeatherCity (){
                 </div>
                 <div>
                     <div>
-                        <p>{sky}</p>
+                        {/* <p>{weatherCity.weather[0].main}</p> */}
                     </div>
                     <div>
                         <img src="" alt="" />
@@ -90,9 +85,9 @@ function WeatherCity (){
                 </div>
                 <div className='weatherCity__week'>
                     {
-                        weather.map((element) => {
-                            return <WeatherCityCard />
-                        })
+                        // weather.map((element) => {
+                        //     return <WeatherCityCard />
+                        // })
                     }
                 </div>
             </div>
