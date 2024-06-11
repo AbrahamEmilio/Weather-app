04565419535ec67acfb6aee36f17ad45d4db632d
AbrahamEmilio <emeilio97@outlook.com>
2024-06-11 11:44:08 -0600

    cambios

--- src/components/WeatherCity/WeatherCity.jsx
+++ src/components/WeatherCity/WeatherCity.jsx
@@ -4,7 +4,6 @@ import WeatherCityCard from './WeatherCityCard/WeatherCityCard.jsx';
 const API_KEY = 'c60df418b9927150faa290e3d8418c82';
 
 import cloudy from '../../assets/clouds.png'
-
 import cloudyCard from '../../assets/clouds.png'
 
 
@@ -18,9 +17,7 @@ function WeatherCity (){
     const [name, setName] = useState('');
     const [country, setCountry] = useState('');
     const [daysWeather, setDaysWeather] = useState([]);
-    const [date, setDate] = useState([]);
-    // const [dayWeek, setDayWeek] = useState([]);
-
+    const [climaDia, setClimaDia] = useState([]);
     
         const getCoordinates = async() => {
             const responseL = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`);
@@ -33,29 +30,53 @@ function WeatherCity (){
             const dataW = await responseW.json();
             setDataWeather(dataW);
 
-            const responseD = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-            const dataD = await responseD.json();
-            
-            setDaysWeather(dataD.list);
-
-            let week = dataD.list;
-            let arrWeek = [];
-
-            week.forEach((element) => {
-                // arrWeek.push(element.dt_txt)
-                let firstArray = element.dt_txt.split(' ');
-                let secondArray = firstArray[0].split('-');
-                arrWeek.push(secondArray[2]);
-            })
-
-            // setDayWeek(arrWeek)
-            setDate(arrWeek)
-
             setName(dataW.name);
             setTemp(Math.trunc(dataW.main.temp - 273) + ' °');
             setTempMax('Max ' + Math.trunc(dataW.main.temp_max - 273) + ' °');
             setTempMin('Min ' + Math.trunc(dataW.main.temp_min - 273) + ' °');
             setCountry(dataW.sys.country);
+
+            getWeatherHouer(lat, lon)
+
+        }
+
+        const getWeatherHouer = async(lat, lon) => {
+            const responseD = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+            const dataD = await responseD.json();
+
+            //Guardamos el array con el clima por hora en DaysWeather
+            setDaysWeather(dataD.list);
+
+            //Creamos un array para almacenar solo los dias de la lista
+            let arrWeek = [];
+
+            //Iteramos sobre el estado donde se encuentran todos los dias y los dividimos para obtener solo el dia en un array nuevo
+            daysWeather.forEach((element) => {
+                let firstArray = element.dt_txt.split(' ');
+                let secondArray = firstArray[0].split('-');
+                arrWeek.push(secondArray[2]);
+            })
+
+            //Creamos un objeto del dia actual y lo guardamos en una estado llamado DateNow
+            const now = new Date();
+            const day = now.getDate().toString();
+
+            let finded = [];
+
+            let idx = arrWeek.indexOf(day);
+            while(idx != -1){
+                finded.push(idx);
+                idx = arrWeek.indexOf(day, idx + 1);
+            }
+
+            const climaHora = []
+
+            finded.forEach((e) => {
+                climaHora.push(daysWeather[e])
+            })
+
+            setClimaDia(climaHora)
+
         }
 
     return(
@@ -97,12 +118,11 @@ function WeatherCity (){
                     <div className={dataWeather ? 'weatherCityWeek__container' : 'hidden'}>
                         <p className='weatherCityWeek__title'>The weather of the week</p>
                         <div className='weatherCityWeek__cards'>
-                            {daysWeather.map((day) => {
+                            {climaDia.map((day) => {
                                 return <WeatherCityCard temp={Math.trunc(day.main.temp - 273) + '°'} day={day.dt_txt} min={'Min ' + Math.trunc(day.main.temp_min - 273) + ' °'} max={'Max ' + Math.trunc(day.main.temp_max - 273) + ' °'} img={cloudyCard} key={day.dt_txt}/>
                             })}
                         </div>
                     </div>
-                    {console.log(date)}
                 </div>
                 <div className='weatherCity__week'>
                 </div>
